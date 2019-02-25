3a088cfaffa7f3dd6252314174a7625b7341d8cd
Sampson Liao <sampsonliao@Sampsons-Mac.local>
2019-02-25 11:20:44 -0800

    added character counter

--- Twitter/TweetViewController.swift
+++ Twitter/TweetViewController.swift
@@ -10,13 +10,29 @@ import RSKPlaceholderTextView
 
 
 
-class TweetViewController: UIViewController {
+class TweetViewController: UIViewController, UITextViewDelegate {
 
     
     
     @IBOutlet weak var tweetTextView: UITextView!
+    
+    
+    @IBOutlet weak var charCountTextView: UILabel!
+    
     var textView: RSKPlaceholderTextView!
     
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        let characterLimit = 140
+        
+        let newText = NSString(string: textView.text!).replacingCharacters(in: range, with: text)
+        self.charCountTextView.text = "\(String(newText.count))/140"
+        if newText.count < characterLimit {
+            self.charCountTextView.textColor = UIColor.black
+        } else {
+            self.charCountTextView.textColor = UIColor.red
+        }
+        return newText.count < characterLimit
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         tweetTextView.becomeFirstResponder()
@@ -24,10 +40,13 @@ class TweetViewController: UIViewController {
         self.textView = RSKPlaceholderTextView(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: 100))
         self.textView.placeholder = "Say something..."
         self.tweetTextView.addSubview(self.textView)
+        
+        textView.delegate = self
+        
+        
         // Do any additional setup after loading the view.
     }
     
-
     @IBAction func cancel(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
